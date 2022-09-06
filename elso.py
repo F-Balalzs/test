64737ebe67efa37dbc9c5b6dc8f8242b2ee080db
F-Balalzs <feketeb@kkszki.hu>
2022-09-06 14:35:03 +0200

    v1.0.2

--- elso.py
+++ elso.py
@@ -1,9 +1,4 @@
 from tkinter import N
 
 
-print("Gimme, gimme, gimme some time to think\
-I'm in the bathroom, looking at me\
-Face in the mirror is all I need (ooh)\
-Wait until the reaper takes my life\
-Never gonna get me out alive\
-I will live a thousand million lives (ooh)")
+print("Gimme, gimme, gimme some time to think\n I'm in the bathroom, looking at me\n Face in the mirror is all I need (ooh)\n Wait until the reaper takes my life\n Never gonna get me out alive\n I will live a thousand million lives (ooh)")
